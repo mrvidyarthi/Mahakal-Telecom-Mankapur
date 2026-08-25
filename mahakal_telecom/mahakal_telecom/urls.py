b82b2edcbea3561dec2e9776351fbfc6a70091fd
kunal gupta <kunalgupta@gmail.com>
2026-08-25 16:18:26 +0530

    fix favicon

--- mahakal_telecom/mahakal_telecom/urls.py
+++ mahakal_telecom/mahakal_telecom/urls.py
@@ -19,10 +19,13 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/media/logo/image.png')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+
 
